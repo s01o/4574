56238eceb5a16fc63277761256e12f2543704562
suchen <7357751@qq.com>
2024-04-18 02:21:18 +0800

    cs2

---
+++ rettm/dbwww/js.js
@@ -0,0 +1,4 @@
+    document.addEventListener("DOMContentLoaded", function() {
+        var leftSide = document.querySelector('.left-side');
+        leftSide.style.backgroundColor = 'blue';
+    });
